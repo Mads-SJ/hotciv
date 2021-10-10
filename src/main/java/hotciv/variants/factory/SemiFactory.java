3b3da8bafc8912049aa17b1a4c7e0bacca1f6541
Mads-SJ <mads.soenderstrup@gmail.com>
2021-10-10 16:58:54 +0200

    FEATURE: SemiCiv

---
+++ src/main/java/hotciv/variants/factory/SemiFactory.java
@@ -0,0 +1,42 @@
+package hotciv.variants.factory;
+
+import hotciv.common.factory.GameFactory;
+import hotciv.common.strategy.*;
+import hotciv.variants.strategy.*;
+
+public class SemiFactory implements GameFactory {
+    @Override
+    public ActionStrategy createActionStrategy() {
+        return new GammaActionStrategy();
+    }
+
+    @Override
+    public AgingStrategy createAgingStrategy() {
+        return new BetaAgingStrategy();
+    }
+
+    @Override
+    public AttackingStrategy createAttackingStrategy() {
+        return new EpsilonAttackingStrategy(new DieRollDecisionStrategy());
+    }
+
+    @Override
+    public WinningStrategy createWinningStrategy() {
+        return new EpsilonWinningStrategy();
+    }
+
+    @Override
+    public WorldLayoutStrategy createWorldLayoutStrategy() {
+        return new DeltaWorldLayoutStrategy();
+    }
+
+    @Override
+    public ValidMoveStrategy createValidMoveStrategy() {
+        return new AlphaValidMoveStrategy();
+    }
+
+    @Override
+    public LegalPositionStrategy createLegalPositionStrategy() {
+        return new AlphaLegalPositionStrategy();
+    }
+}
